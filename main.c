6a5b8cd473e33fbbb1f3ca48d6fcc738e7f14ba5
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-11-15 16:35:02 +0900

    11-5

--- main.c
+++ main.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void)
 {
-	int i;
-	int grade[5];
-	int *ptr;
-	ptr= grade;
-	
-	int sum;
-	double avg;
-	
-	for (i=0;i<5;i++){
-		printf("input grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
-	
-	i =0;
-	while(*ptr){
-		printf("grade[%d] = %d\n", i, *ptr);
-		sum += *ptr;
-		ptr ++;
-		i ++;
-	}
-	avg = sum/5.0;
-	printf("average: %.2lf\n", avg);
+	char *pc;
+	int *pi;
+	double *pd;
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	return 0;
 }

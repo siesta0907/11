7371624ad28790b52920e3d3ad6e12ee90864f55
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-11-15 17:26:09 +0900

    11-7

--- main.c
+++ main.c
@@ -2,15 +2,26 @@
 
 char *proverb="All that glisters is not gold.";
 
-void setPointer(char *q)
+void setPointer(char **q)
 {
-	q = proverb;
+	/*printf("q: %p\n", q);
+	printf("q: %s\n", q);
+	printf("*q: %p\n", *q);
+	
+	printf("proverb: %p\n", proverb);
+	printf("proverb: %s\n", proverb); */
+
+	*q = proverb;
 }
 	
 int main(void)
 {
 	char *p = "zzz";
-	setPointer(p);
-	printf("%s \n", p);
+/*rintf("p: %p\n", p);
+	printf("p: %s\n", p);
+	printf("*p: %p\n", *p); */
+	
+	setPointer(&p);
+	printf("res: %s\n", p);
 	return 0;
 }
